641566bcfcb77b8037a8795eab6c8e5c004e2324
315920958@qq.com <315920958@qq.com>
2019-11-12 16:17:04 +0800

    初步跑通

---
+++ src/main/java/org/dragonli/service/modules/user/interfaces/UserService.java
@@ -0,0 +1,47 @@
+package org.dragonli.service.modules.user.interfaces;
+
+import java.util.List;
+import java.util.Map;
+
+public interface UserService {
+
+	Map<String, Object> registUser(String username, String password) throws Exception;
+
+	Map<String, Object> registUser(String username, String password, String recommendCode,
+			Long countryId) throws Exception ;
+
+	Map<String, Object> registUser(String username, String password, String recommendCode, Long countryId,
+			String email, String phone, String nickname, String passwdCode,
+			Map<String, Object> expendsParas) throws Exception;
+
+	Map<String, Object> login(String username, String encryptedPasswd) throws Exception ;
+
+	Map<String, Object> loginByCode(String username, String code) throws Exception;
+
+	Map<String, Object> getUserById(Long id);
+
+	List<Map<String, Object>> getUserList(List<Long> idList);
+
+	Map<String, Object> findUser(Object key);
+
+	Map<String, Object> resetPasswdById(Long id, String code) throws Exception ;
+
+	Map<String, Object> resetPasswdByKey(String key, String code) throws Exception ;
+
+	Map<String, Object> changePassword(Long id, String password, String newpw, String passwdCode,
+			Boolean dontValicodeOld) throws Exception;
+
+
+	Map<String, Object> changeEmail(Long id, String code, String newEmail, Boolean setEmailValidated) throws Exception;
+
+	Map<String, Object> changePhone(Long id, String code, String newPhone, Boolean setPhoneValidated) throws Exception ;
+
+	Map<String, Object> generateValidateCodeByUserId(Long id) throws Exception ;
+
+	Map<String, Object> generateValidateCodeByUserId(String key) throws Exception;
+	
+
+	Map<String, Object> generateLoginCode(String key);
+
+
+}
